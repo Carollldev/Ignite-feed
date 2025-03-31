5c5a52c04a93bfd6b5f301eb3649d3f06ee8f225
Carollldev <caroline.medsilva10@gmail.com>
2025-03-31 22:06:45 +0000

    Estrutura da aplicação - CSS Modules

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import { Header } from "./components/Header";
+import {Post} from "./Post"
+
+import './styles.css';
+
+export function App() {
+
+  return (
+  <div>
+    <Header />
+    
+    <Post   
+     author="Caroline Medeiros"
+     content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi non minus maxime cupiditate deleniti ratione magnam numquam nam aperiam eligendi exercitationem repellat minima accusantium, fugit eos tempora quaerat tempore autem!"
+  />
+  <Post
+    author="Renato Gabriel"
+    content="Um novo post muito legal"
+  />
+  </div> 
+  )
+}
