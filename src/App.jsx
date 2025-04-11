8a24e956ed184ad470bda1d833b889b6d9cea961
Carollldev <caroline.medsilva10@gmail.com>
2025-04-11 18:58:17 +0000

    Os motores do React - Propriedades do Post

--- src/App.jsx
+++ src/App.jsx
@@ -19,21 +19,21 @@ const posts = [
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
       { type: 'link', content: 'jane.design/doctorcare' },
     ],
-    publishedAt: new Date('2022-05-03 20:00:00'),
+    publishedAt: new Date('2025-04-10 20:00:00'),
   },
   {
     id: 2,
     author: {
-      avatarUrl: 'https://github.com/maykbrito.png',
-      name: 'Mayk Brito',
-      role: 'Educator @Rocketseat'
+      avatarUrl: 'https://github.com/RenatoNascimentoDev.png',
+      name: 'Renato Nascimento',
+      role: 'Back-End Developer'
     },
     content: [
       { type: 'paragraph', content: 'Fala galera 👋' },
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
       { type: 'link', content: 'jane.design/doctorcare' },
     ],
-    publishedAt: new Date('2022-05-10 20:00:00'),
+    publishedAt: new Date('2025-04-10 20:00:00'),
   },
 ];
 
